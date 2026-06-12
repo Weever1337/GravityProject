3dbe4764316eadf34de6e5de7390f3f6270b3706
Weever <109693401+Weever1337@users.noreply.github.com>
2026-06-12 18:03:22 +0300

    fixed shift offset

--- src/main/java/org/weever/gravitymod/mixin/client/GravityPlayerRendererMixin.java
+++ src/main/java/org/weever/gravitymod/mixin/client/GravityPlayerRendererMixin.java
@@ -7,12 +7,27 @@ import net.minecraft.util.Direction;
 import net.minecraft.util.math.vector.Vector3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.weever.gravitymod.util.GravityAPI;
 import org.weever.gravitymod.util.RotationUtil;
 
 @Mixin(PlayerRenderer.class)
 public abstract class GravityPlayerRendererMixin {
+    // crouch rotation fix
+    @Inject(
+            method = "getRenderOffset(Lnet/minecraft/client/entity/player/AbstractClientPlayerEntity;F)Lnet/minecraft/util/math/vector/Vector3d;",
+            at = @At("RETURN"),
+            cancellable = true
+    )
+    private void gravitymod$rotateRenderOffset(AbstractClientPlayerEntity player, float partialTicks, CallbackInfoReturnable<Vector3d> cir) {
+        Direction gravityDirection = GravityAPI.getGravityDirection(player);
+        if (gravityDirection == Direction.DOWN) return;
+
+        cir.setReturnValue(RotationUtil.vecPlayerToWorld(cir.getReturnValue(), gravityDirection));
+    }
+
     /**elytra rotation fix*/
     @ModifyVariable(
             method = "setupRotations*",
