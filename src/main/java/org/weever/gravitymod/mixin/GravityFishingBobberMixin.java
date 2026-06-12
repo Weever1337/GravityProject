8bb71c91fbb02658a4d22fe8957506521ee3f86d
Weever <109693401+Weever1337@users.noreply.github.com>
2026-06-12 16:00:42 +0300

    fixed fishing hook

--- src/main/java/org/weever/gravitymod/mixin/GravityFishingBobberMixin.java
+++ src/main/java/org/weever/gravitymod/mixin/GravityFishingBobberMixin.java
@@ -3,19 +3,49 @@ package org.weever.gravitymod.mixin;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.FishingBobberEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.weever.gravitymod.util.GravityAPI;
 import org.weever.gravitymod.util.RotationUtil;
 
+import javax.annotation.Nullable;
+
 @Mixin(FishingBobberEntity.class)
 public abstract class GravityFishingBobberMixin {
+    @Shadow
+    private Entity hookedIn;
+
+    @Inject(
+            method = "bringInHookedEntity",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void gravitymod$bringInHookedEntity(CallbackInfo ci) {
+        Entity owner = ((ProjectileEntity) (Object) this).getOwner();
+        Entity hooked = this.hookedIn;
+        if (owner == null || hooked == null) return;
+
+        Direction hookedGravity = GravityAPI.getGravityDirection(hooked);
+        if (hookedGravity == Direction.DOWN) return;
+
+        ci.cancel();
+        Entity self = (Entity) (Object) this;
+        Vector3d worldPull = new Vector3d(
+                owner.getX() - self.getX(),
+                owner.getY() - self.getY(),
+                owner.getZ() - self.getZ()).scale(0.1D);
+        hooked.setDeltaMovement(hooked.getDeltaMovement()
+                .add(RotationUtil.vecWorldToPlayer(worldPull, hookedGravity)));
+    }
+
     @Inject(
             method = "<init>(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/world/World;II)V",
             at = @At("TAIL")
